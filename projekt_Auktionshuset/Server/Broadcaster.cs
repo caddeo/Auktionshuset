26b2dc1f672a0181e259145e30f49ab9486c1c91
Patrick Caddeo Nielsen <poss_en@hotmail.com>
2015-09-11 11:07:08 +0200

    Serveren virker ikke af en grund

---
+++ projekt_Auktionshuset/Server/Broadcaster.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Broadcaster
+    {
+        public delegate void BroadcastEvent(string message);
+
+        public event BroadcastEvent BroadcastMessage;
+
+
+        private List<ClientHandler> _clients;
+
+        public Broadcaster()
+        {
+            this._clients = new List<ClientHandler>();
+        }
+
+        /* Tilmeld fra listen */
+        public void Tilmeld(ClientHandler client)
+        {
+            _clients.Add(client);
+        }
+
+        /* Afmeld fra listen */
+        public void Afmeld(ClientHandler client)
+        {
+            _clients.Remove(client);
+        }
+
+        /* Broadcast til clienter på listen */
+        public void Broadcast(string message)
+        {
+            if (this.BroadcastMessage != null)
+            {
+                BroadcastMessage(message);
+            }
+        }
+    }
+}
---
+++ projekt_Auktionshuset/Server/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+
+        private Broadcaster _broadcaster;
+
+        private bool running;
+
+        public Server(int port, string ip)
+        {
+            _broadcaster = new Broadcaster();
+
+            IPAddress IP = IPAddress.Parse(ip);
+            TcpListener listener = new TcpListener(IP, port);
+
+            running = true;
+
+            listener.Start();
+
+            while (running)
+            {
+                System.Console.WriteLine("Server klar");
+                Socket clientSocket = listener.AcceptSocket();
+
+                System.Console.WriteLine("Connection");
+                ClientHandler handler = new ClientHandler(clientSocket, _broadcaster);
+
+                Thread clientThread = new Thread(handler.RunClient);
+            }
+
+            
+        }
+
+
+    }
+}
