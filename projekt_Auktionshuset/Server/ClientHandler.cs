2ec20c314cb007062265c2922795aea52d30c0ee
Patrick Caddeo Nielsen <poss_en@hotmail.com>
2015-09-17 13:31:36 +0200

    Server færdig

--- projekt_Auktionshuset/Server/ClientHandler.cs
+++ projekt_Auktionshuset/Server/ClientHandler.cs
@@ -35,12 +35,11 @@ namespace Server
 
             /* Udskriver IP'en */
             string clientIp = clientSocket.RemoteEndPoint.ToString();
-            IPAddress ip = IPAddress.Parse(clientIp.Substring(0, clientIp.Length - 5));
 
-            client = new Client(clientIp.Substring(0, clientIp.Length - 5));
+            client = new Client(clientIp);
             _broadcaster.Clients.Add(client);
 
-            Console.WriteLine(clientIp +" connected");
+            Console.WriteLine(clientIp +"("+client.Name+") connected");
         }
 
         public void RunClient()
@@ -61,6 +60,9 @@ namespace Server
             }
 
             /* Disconnect */
+            string clientIp = _clientSocket.RemoteEndPoint.ToString();
+            Console.WriteLine(clientIp+" disconnected");
+
             this._netStream.Close();
             this._writer.Close();
             this._reader.Close();
@@ -88,7 +90,8 @@ namespace Server
             {
                 /* De forskellige exceptions kan catches*/
                 /* retunere fejl beskeden */
-                return "Recieve() "+ e.Message;
+                Console.WriteLine(e.Message);
+                return null;
             }
         }
 
@@ -107,16 +110,20 @@ namespace Server
                 while (_running)
                 {
                     /* Imens den kører så handle user input*/
-                    HandleInput();
+                    if (_auction != null)
+                    {
+                        HandleInput();
+                    }
+                    else
+                    {
+                        Send("MESSAGE");
+                        Send("Ingen auktion kørende - lukker forbindelse");
+                        Send("DISCONNECT");
+                    }
                 }
             }
             catch (Exception e)
             {
-                //if (e is NullReferenceException)
-                //{
-                //    // ignore
-                //}
-
                 Console.WriteLine("Run() " + e.Message);
             }
             finally
@@ -127,8 +134,6 @@ namespace Server
             }
         }
 
-
-
         private void HandleInput() 
         {
             // Behandling af input fra klient
@@ -137,20 +142,14 @@ namespace Server
             // Tjekker om input er null (den er typisk null når man tryker kryds)
             if (input != null)
             {
-                /*if (input.Trim().ToLower() == "usercount")
-
-                {
-                    _broadcaster.Broadcast(_broadcaster.BroadcastClientCount());
-                }*/
 
                 switch (input.Trim().ToUpper())
                 {
                     case "CONNECTED":
                         Send("DESCRIPTION");
+                        Send(_auction.Name);
                         Send(_auction.Description);
-                        Send("ESTIMATEDPRICE");
-                        Send(_auction.CurrentPrice.ToString());
-                        Send("CURRENTPRICE");
+                        Send(_auction.EstimatedPrice.ToString());
                         Send(_auction.CurrentPrice.ToString());
                         break;
                     case "BID":
@@ -178,21 +177,39 @@ namespace Server
             {
                 double clientBid = double.Parse(bid);
 
-                if (clientBid > _auction.CurrentPrice)
+                if (_auction.TimeLeft > 0)
                 {
-                    // bud er ok
-                    client.SetCurrentBid(clientBid);
-                    _auction.SetHighestBid(client, clientBid);
+                    if (clientBid > _auction.CurrentPrice)
+                    {
+                        if (_auction.HighestBidder == null) _auction.Timer.Start();
 
-                    _broadcaster.Broadcast("NEWBIDDER");
-                    _broadcaster.Broadcast(client.Name + " er den nye højeste bydende (" + clientBid + " kr.)");
-                    _broadcaster.Broadcast("NEWHIGHEST");
-                    _broadcaster.Broadcast(clientBid.ToString());
+                        // bud er ok
+                        client.SetCurrentBid(clientBid);
+                        _auction.SetHighestBid(client, clientBid);
+
+                        _broadcaster.Broadcast("NEWBIDDER");
+                        _broadcaster.Broadcast("Client " + client.Name + " er den nye højeste bydende (" + clientBid +
+                                               " kr.)");
+                        _broadcaster.Broadcast("NEWHIGHEST");
+                        _broadcaster.Broadcast(clientBid.ToString());
+
+                        Console.WriteLine("Client " + client.Name + " er den nye højeste bydende (" + clientBid +
+                                          " kr.)");
+
+                        // start timer med 10 sekunder
+                        _auction.SetTimeLeft(17000); // reset
+                    }
+                    else
+                    {
+                        // bud er ikke ok
+                        Send("MESSAGE");
+                        Send("Dit bud er ikke over " + _auction.CurrentPrice + " kr!");
+                    }
                 }
                 else
                 {
-                    // bud er ikke ok
-                    Send("Dit bud er ikke over "+_auction.CurrentPrice+" kr!");
+                    Send("MESSAGE");
+                    Send("Auktionen er udløbet. Den blev solgt til "+_auction.HighestBidder.Name+" for "+_auction.CurrentPrice);
                 }
             }
             catch(Exception e) 
